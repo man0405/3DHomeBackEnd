548c3053d756d1f791b96701c47fac86fb50d4bf
Thong Van <vuongvanthong113@gmail.com>
2023-12-08 01:38:19 +0700

    thay exception

--- src/main/java/com/example/backend/services/Impl/CustomerServiceImpl.java
+++ src/main/java/com/example/backend/services/Impl/CustomerServiceImpl.java
@@ -4,11 +4,13 @@ import com.example.backend.convertDTO.CustomerConvert;
 import com.example.backend.dto.CustomerPassword;
 import com.example.backend.dto.CustomerPhone;
 import com.example.backend.dto.CustomerProfile;
+import com.example.backend.exception.CustomMessageException;
 import com.example.backend.models.entity.Customer;
 import com.example.backend.repository.CustomerRepository;
 import com.example.backend.services.CustomerService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.regex.Matcher;
@@ -40,13 +42,17 @@ public class CustomerServiceImpl implements CustomerService {
     @Override
     public CustomerProfile updateProfile(CustomerProfile customerProfile) {
         Customer oldCustomer = customerRepository.findById(customerProfile.getId()).orElseThrow(
-                ()-> new IllegalStateException("Customer with id = "+customerProfile.getId()+" does not exists"));
+                ()-> new CustomMessageException("Customer with id = "+customerProfile.getId()+" does not exists", String.valueOf(HttpStatus.BAD_REQUEST)));
         Customer newCustomer = new Customer();
 
         newCustomer = customerConvert.toCustomer(customerProfile,oldCustomer);
 
-        if (newCustomer.getFirstName()!=null && newCustomer.getLastName()!=null && !newCustomer.getFirstName().equals(oldCustomer.getFirstName()) && !newCustomer.getLastName().equals(oldCustomer.getLastName()))
+        if (newCustomer.getFirstName()!=null && newCustomer.getLastName()!=null && !newCustomer.getFirstName().equals(oldCustomer.getFirstName()) && !newCustomer.getLastName().equals(oldCustomer.getLastName())){
             newCustomer = customerRepository.save(newCustomer);
+        } else {
+            throw new CustomMessageException("Customer with name does not null", String.valueOf(HttpStatus.BAD_REQUEST));
+        }
+
         return customerConvert.toCustomerProfile(newCustomer);
     }
     @Transactional(rollbackOn = {Exception.class, Throwable.class})
@@ -54,7 +60,7 @@ public class CustomerServiceImpl implements CustomerService {
     public CustomerPhone updatePhone(CustomerPhone customerPhone) {
         Customer newCustomer = new Customer();
         Customer oldCustomer = new Customer();
-        oldCustomer = customerRepository.findById(customerPhone.getId()).orElseThrow(()-> new IllegalStateException("ID = "+customerPhone.getId()+" does not exists"));
+        oldCustomer = customerRepository.findById(customerPhone.getId()).orElseThrow(()-> new CustomMessageException("Customer with id = "+customerPhone.getId()+" does not exists", String.valueOf(HttpStatus.BAD_REQUEST)));
         newCustomer = customerConvert.toCustomer(customerPhone,oldCustomer);
 
         if (isValidPhoneNumber(newCustomer.getPhone())){
@@ -70,7 +76,7 @@ public class CustomerServiceImpl implements CustomerService {
     public CustomerPassword updatePassword(CustomerPassword customerPassword) {
         Customer newCustomer = new Customer();
         Customer oldCustomer = new Customer();
-        oldCustomer = customerRepository.findById(customerPassword.getId()).orElseThrow(()-> new IllegalStateException("ID = "+customerPassword.getId()+" does not exists"));;
+        oldCustomer = customerRepository.findById(customerPassword.getId()).orElseThrow(()-> new CustomMessageException("Customer with id = "+customerPassword.getId()+" does not exists", String.valueOf(HttpStatus.BAD_REQUEST)));
         String password = oldCustomer.getUser().getPassword();
         if (isCorrectPassword(password, customerPassword)){
             newCustomer = customerConvert.toCustomer(customerPassword,oldCustomer);
