eb0a2b6fbbf59a7d6beab503cc2c52495e21064b
man0405 <vanman160504@gmail.com>
2023-12-11 23:02:38 +0700

    info response

--- src/main/java/com/example/backend/controllers/CustomerController.java
+++ src/main/java/com/example/backend/controllers/CustomerController.java
@@ -3,13 +3,10 @@ package com.example.backend.controllers;
 import com.example.backend.dto.CustomerPassword;
 import com.example.backend.dto.CustomerPhone;
 import com.example.backend.dto.CustomerProfile;
+import com.example.backend.dto.InfoResponse;
+import com.example.backend.models.entity.Customer;
 import com.example.backend.services.CustomerService;
-import com.example.backend.services.information.JwtService;
-import com.example.backend.util.ExtractId;
-import jakarta.servlet.http.Cookie;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,27 +14,38 @@ import static com.example.backend.util.ExtractId.ExtractIdFromToken;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("api/v1")
 public class CustomerController {
 
     private final
     CustomerService customerService;
     private PasswordEncoder passwordEncoder;
 
+    @GetMapping(value ="info" )
+    public InfoResponse getProfile(@CookieValue("uss") String cookie ){
+        Customer customer = customerService.findByUser_Id(ExtractIdFromToken(cookie));
+        return InfoResponse
+                .builder()
+                .phone(customer.getPhone())
+                .name(customer.getFirstName() +" "+  customer.getLastName())
+                .build();
+    }
 
 
-    @PutMapping(value = "api/v1/profile")
+
+    @PutMapping(value = "profile")
     public CustomerProfile updateProfile(@RequestBody CustomerProfile customerProfile, @CookieValue("uss") String cookie){
         System.out.println("Cookie: "  + cookie);
 //        customerProfile.setId(getID(cookie));
         customerProfile.setId(ExtractIdFromToken(cookie));
         return customerService.updateProfile(customerProfile);
     }
-    @PutMapping(value = "api/v1/phone")
+    @PutMapping(value = "phone")
     public CustomerPhone updatePhone(@RequestBody CustomerPhone customerPhone, @CookieValue("uss") String cookie){
         customerPhone.setId(ExtractIdFromToken(cookie));
         return customerService.updatePhone(customerPhone);
     }
-    @PutMapping(value = "api/v1/password")
+    @PutMapping(value = "password")
     public CustomerPassword updatePassword(@RequestBody CustomerPassword customerPassword, @CookieValue("uss") String cookie){
         customerPassword.setId(ExtractIdFromToken(cookie));
         System.out.println("customer password" + customerPassword);
